f966d34c625971f63b707429769803465cc2664f
Wouter Gritter <woutergritter@gmail.com>
2023-01-06 12:59:34 +0100

    Fix history config not loading after having been on the page for a while

--- rboit-front/src/app/device/device-history-config.service.ts
+++ rboit-front/src/app/device/device-history-config.service.ts
@@ -1,20 +1,16 @@
 import { Injectable } from '@angular/core';
-import {Subject} from "rxjs";
 import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class DeviceHistoryConfigService {
-  private cachedHistoryConfig: Subject<DeviceHistoryConfig> = new Subject<DeviceHistoryConfig>();
 
   constructor(private http: HttpClient) {
-    this.http.get<DeviceHistoryConfig>('/api/device/historyConfig')
-      .subscribe(config => this.cachedHistoryConfig.next(config));
   }
 
   getHistoryConfig() {
-    return this.cachedHistoryConfig.asObservable();
+    return this.http.get<DeviceHistoryConfig>('/api/device/historyConfig');
   }
 }
 
