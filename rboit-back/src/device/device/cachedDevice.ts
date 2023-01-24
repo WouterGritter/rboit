82923c00d440989dc4ead756cb7c438b6fab0a92
Wouter Gritter <woutergritter@gmail.com>
2023-01-24 22:33:03 +0100

    Implement deviceErrorMonitor.ts and call onDeviceError() when an error is caught

--- rboit-back/src/device/device/cachedDevice.ts
+++ rboit-back/src/device/device/cachedDevice.ts
@@ -1,4 +1,5 @@
 import {Device, DeviceType} from "./device";
+import {onDeviceError} from "../deviceErrorMonitor";
 
 const DEVICE_CACHE_MAX_AGE = parseInt(process.env.DEVICE_CACHE_MAX_AGE);
 console.log(`DEVICE_CACHE_MAX_AGE=${DEVICE_CACHE_MAX_AGE}`);
@@ -32,6 +33,8 @@ export abstract class CachedDevice<T> implements Device<T> {
             .catch(reason => {
                 console.log(`Error on first request of device ${this.name}: ${reason}`);
                 console.log('Retrying in 5 seconds.');
+                onDeviceError(this.name, reason);
+
                 setTimeout(() => this.performReadyRequest(), 5000);
             });
     }
@@ -48,10 +51,11 @@ export abstract class CachedDevice<T> implements Device<T> {
             }
 
             this.fetching = true;
-            try{
+            try {
                 this.cachedReading = await retry(() => this.getActualReading());
-            }catch(e) {
+            } catch (e) {
                 this.fetching = false;
+                onDeviceError(this.name, e);
                 return Promise.reject(e);
             }
 
@@ -78,7 +82,7 @@ async function retry<T>(task: () => Promise<T>, maxTries: number = 3): Promise<T
     while (true) {
         try {
             return await task();
-        }catch(e) {
+        } catch (e) {
             tries++;
             if (tries == maxTries) {
                 throw e;
---
+++ rboit-back/src/device/deviceErrorMonitor.ts
@@ -0,0 +1,74 @@
+import {discordClient} from "../discordClient";
+
+const MAX_AGE = 1000 * 60 * 60; // 1 hour
+
+const WARNING_MESSAGE_INTERVAL = 1000 * 60 * 60 * 24; // 1 day
+
+const ERROR_WARNING_THRESHOLD = 1;
+
+const errorEntries: { [deviceName: string]: undefined | ErrorEntry[] } = {};
+const lastWarningDates: { [deviceName: string]: undefined | Date } = {};
+
+export function onDeviceError(deviceName: string, error: any): void {
+    let entries = errorEntries[deviceName];
+    if (entries === undefined) {
+        entries = [];
+        errorEntries[deviceName] = entries;
+    }
+
+    const entry = {
+        date: new Date(),
+        error: error,
+    };
+    entries.push(entry);
+
+    cleanUpEntries(entries);
+    if (shouldSendWarningMessage(deviceName, entries)) {
+        sendWarningMessage(deviceName, entries, entry);
+    }
+}
+
+function cleanUpEntries(entries: ErrorEntry[]): void {
+    const now = new Date();
+    for (let i = entries.length - 1; i >= 0; i--) {
+        const entry = entries[i];
+        const age = now.getTime() - entry.date.getTime();
+        if (age > MAX_AGE) {
+            entries.splice(i, 1);
+        }
+    }
+}
+
+function shouldSendWarningMessage(deviceName: string, entries: ErrorEntry[]): boolean {
+    if (entries.length < ERROR_WARNING_THRESHOLD) {
+        // Not enough errors
+        return false;
+    }
+
+    const lastWarningDate = lastWarningDates[deviceName];
+    if (lastWarningDate !== undefined && new Date().getTime() - lastWarningDate.getTime() < WARNING_MESSAGE_INTERVAL) {
+        // Not enough time between last warning message
+        return false;
+    }
+
+    return true;
+}
+
+function sendWarningMessage(deviceName: string, entries: ErrorEntry[], lastError: ErrorEntry): void {
+    lastWarningDates[deviceName] = new Date();
+
+    let message = `:warning: High error count for device ${deviceName}! Received ${entries.length} errors in the last hour. Error message: ${String(lastError.error)}`;
+
+    if (lastError.error.stack !== undefined) {
+        message += '\n```\n' + lastError.error.stack + '\n```';
+    }
+
+    (async () => {
+        await discordClient.send(message);
+    })();
+}
+
+declare type ErrorEntry = {
+    date: Date,
+    error: any,
+};
