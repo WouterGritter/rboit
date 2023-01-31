75654f023fce9e778f2b2fb8d4bacbd7943f16e2
Wouter Gritter <woutergritter@gmail.com>
2023-01-31 15:30:46 +0100

    Connect to redis at startup

--- rboit-back/src/index.ts
+++ rboit-back/src/index.ts
@@ -7,17 +7,21 @@ import {router} from "./router";
 import {startGoveeBatteryMonitor} from "./goveeBatteryMonitor";
 import {startRukbunkerEnergyLogger} from "./rukbunkerEnergyLogger";
 import {startRukbunkerSolarEnergyLogger} from "./rukbunkerSolarEnergyLogger";
+import {ensureRedisConnected} from "./redisClient";
 
+(async () => {
+    await ensureRedisConnected();
 
-const app = express();
-const port = process.env.PORT || '80';
+    const app = express();
+    const port = process.env.PORT || '80';
 
-app.use(router);
+    app.use(router);
 
-app.listen(port, () => {
-    console.log(`Listening on http://0.0.0.0:${port}`);
-});
+    app.listen(port, () => {
+        console.log(`Listening on http://0.0.0.0:${port}`);
+    });
 
-startGoveeBatteryMonitor();
-startRukbunkerEnergyLogger();
-startRukbunkerSolarEnergyLogger();
+    startGoveeBatteryMonitor();
+    startRukbunkerEnergyLogger();
+    startRukbunkerSolarEnergyLogger();
+})();
--- rboit-back/src/redisClient.ts
+++ rboit-back/src/redisClient.ts
@@ -4,11 +4,12 @@ const redisClient = createClient({
     url: process.env.REDIS_URL
 })
 
-async function ensureConnected() {
+export async function ensureRedisConnected() {
     if (redisClient.isReady) {
         return;
     }
 
+    console.log('Connecting to redis...');
     try{
         await redisClient.disconnect()
     }catch(ignored) { }
@@ -19,7 +20,7 @@ async function ensureConnected() {
 }
 
 export async function redisGet<T>(key: string): Promise<T | undefined> {
-    await ensureConnected();
+    await ensureRedisConnected();
 
     const value = await redisClient.get(key as any);
     if (value === null || value === undefined) {
@@ -30,7 +31,7 @@ export async function redisGet<T>(key: string): Promise<T | undefined> {
 }
 
 export async function redisSet<T>(key: string, value: T): Promise<void> {
-    await ensureConnected();
+    await ensureRedisConnected();
 
     await redisClient.set(key as any, JSON.stringify(value) as any);
 }
