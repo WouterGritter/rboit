49f41b48ba8437cf3916603200381939d2e6dd99
Wouter Gritter <woutergritter@gmail.com>
2023-01-31 15:25:47 +0100

    Use redis to store lastWattHours

--- rboit-back/src/rukbunkerSolarEnergyLogger.ts
+++ rboit-back/src/rukbunkerSolarEnergyLogger.ts
@@ -1,34 +1,34 @@
 import {DEVICE_REPOSITORY} from "./device/deviceRepository";
 import {discordClient} from "./discordClient";
 import {RukbunkerSolarPowerDevice, RukbunkerSolarReading} from "./device/device/power/rukbunkerSolarPowerDevice";
+import {redisGet, redisSet} from "./redisClient";
+import {sleep} from "./util/sleep";
 
-let lastWattHours = 0;
+export async function startRukbunkerSolarEnergyLogger(): Promise<void> {
+    if (await getLastWattHours() === undefined) {
+        await sleep(30 * 1000); // Wait for device to be ready...
 
-export function startRukbunkerSolarEnergyLogger(): void {
-    setTimeout(() => {
-        getTotalRBSolarWattHours()
-            .then(wattHours => {
-                lastWattHours = wattHours;
+        console.log('Last rukbunker total solar energy generation was not present in redis. Loading value from device...');
+        const totalWattHours = await getTotalRBSolarWattHours();
+        await setLastWattHours(totalWattHours);
+    }
 
-                const nextMonth = getNextDayDate();
-                const millisUntilNextMonth = nextMonth.getTime() - new Date().getTime();
-                console.log(`Logging rukbunker solar energy generation in ${millisUntilNextMonth} milliseconds.`);
+    const nextMonth = getNextDayDate();
+    const millisUntilNextMonth = nextMonth.getTime() - new Date().getTime();
+    console.log(`Logging rukbunker solar energy generation in ${millisUntilNextMonth} milliseconds.`);
 
-                setTimeout(logRukbunkerSolarGeneration, millisUntilNextMonth);
-            })
-    }, 1000 * 60);
+    setTimeout(logRukbunkerSolarGeneration, millisUntilNextMonth);
 }
 
 async function logRukbunkerSolarGeneration(tryNum: number = 0) {
     try {
         const totalWattHours = await getTotalRBSolarWattHours();
-        const wattHoursToday = totalWattHours - lastWattHours;
-        lastWattHours = totalWattHours;
+        const wattHoursToday = totalWattHours - await getLastWattHours();
 
         const message = `:sunny: Rukbunker generation today: \`${wattHoursToday}\` Wh`;
-
         await discordClient.send(message);
 
+        await setLastWattHours(totalWattHours);
         startRukbunkerSolarEnergyLogger();
     } catch (e) {
         if (tryNum < 5) {
@@ -50,4 +50,15 @@ async function getTotalRBSolarWattHours(): Promise<number> {
     const reading = await device.getReading();
     const solarReading = reading.source as RukbunkerSolarReading;
     return solarReading.wattHours;
+}
+
+async function getLastWattHours(): Promise<number | undefined> {
+    const value = await redisGet<number>('rb-solar-last-watt-hours');
+    console.log(`Loaded last rukbunker total solar energy generation from redis: ${value / 1000} kWh`);
+    return value;
+}
+
+async function setLastWattHours(value: number): Promise<void> {
+    await redisSet<number>('rb-solar-last-watt-hours', value);
+    console.log(`Stored last rukbunker total solar energy generation to redis: ${value / 1000} kWh`);
 }
