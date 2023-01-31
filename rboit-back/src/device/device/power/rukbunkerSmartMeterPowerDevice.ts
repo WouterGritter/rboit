45208d3b463ea4d5e5b889b3b048e3716268bbf6
Wouter Gritter <woutergritter@gmail.com>
2023-01-31 14:48:17 +0100

    Revert "Try out a mechanism for detecting redelivery"
    This reverts commit 8b64312b0d76c93cb5e451160b746461b8a27a53.

--- rboit-back/src/device/device/power/rukbunkerSmartMeterPowerDevice.ts
+++ rboit-back/src/device/device/power/rukbunkerSmartMeterPowerDevice.ts
@@ -1,8 +1,6 @@
 import {CachedDevice} from "../cachedDevice";
 import {PowerReading, PowerReadingValues} from "./powerReading";
 import {DeviceType} from "../device";
-import {RukbunkerSolarPowerDevice, RukbunkerSolarReading} from "./rukbunkerSolarPowerDevice";
-import {DEVICE_REPOSITORY} from "../../deviceRepository";
 
 export class RukbunkerSmartMeterPowerDevice extends CachedDevice<PowerReading> {
     readonly history: PowerReading[] = [];
@@ -10,13 +8,9 @@ export class RukbunkerSmartMeterPowerDevice extends CachedDevice<PowerReading> {
     readonly type: DeviceType = 'power';
 
     async getActualReading(): Promise<PowerReading> {
-        const reading = await fetch(`${process.env.PYTHON_DAEMON}/dts353f`)
-            .then(response => response.json() as Promise<DTS353FReading>);
-
-        const solarDevice = DEVICE_REPOSITORY.findDevice('rb-solar', 'power') as RukbunkerSolarPowerDevice;
-        const solarReading = (await solarDevice.getActualReading()).source as RukbunkerSolarReading;
-
-        return this.toPowerReading(reading, solarReading);
+        return fetch(`${process.env.PYTHON_DAEMON}/dts353f`)
+            .then(response => response.json() as Promise<DTS353FReading>)
+            .then(readings => this.toPowerReading(readings));
     }
 
     private toPowerReadingValues(reading: DTS353FReading, phase: 'l1' | 'l2' | 'l3'): PowerReadingValues {
@@ -27,20 +21,10 @@ export class RukbunkerSmartMeterPowerDevice extends CachedDevice<PowerReading> {
         return {power, voltage, amperage};
     }
 
-    private toPowerReading(reading: DTS353FReading, solarReading: RukbunkerSolarReading): PowerReading {
+    private toPowerReading(reading: DTS353FReading): PowerReading {
         const L1 = this.toPowerReadingValues(reading, 'l1');
         const L2 = this.toPowerReadingValues(reading, 'l2');
         const L3 = this.toPowerReadingValues(reading, 'l3');
-
-        const L3_standbyPower = 41;
-
-        const solarPower = Math.floor(solarReading.calculatedWattage);
-        const solarPowerThatCouldBeRedelivered = solarPower - L3_standbyPower;
-        if (Math.round(L3.power) <= solarPowerThatCouldBeRedelivered) {
-            // Could be regenerating, so assume that we are...
-            L3.power *= -1;
-            L3.amperage *= -1;
-        }
 
         const total = {
             power: L1.power + L2.power + L3.power,
