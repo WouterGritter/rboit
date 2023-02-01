31d18304156ff8e871de33d251b6a83f7d05666d
Wouter Gritter <woutergritter@gmail.com>
2023-02-01 22:54:33 +0100

    Try out another method for detecting redelivery

--- rboit-back/src/device/device/power/rukbunkerSmartMeterPowerDevice.ts
+++ rboit-back/src/device/device/power/rukbunkerSmartMeterPowerDevice.ts
@@ -1,6 +1,8 @@
 import {CachedDevice} from "../cachedDevice";
 import {PowerReading, PowerReadingValues} from "./powerReading";
 import {DeviceType} from "../device";
+import {AndledonSmartMeterPowerDevice, AndledonSmartMeterReading} from "./andledonSmartMeterPowerDevice";
+import {DEVICE_REPOSITORY} from "../../deviceRepository";
 
 export class RukbunkerSmartMeterPowerDevice extends CachedDevice<PowerReading> {
     readonly history: PowerReading[] = [];
@@ -8,9 +10,15 @@ export class RukbunkerSmartMeterPowerDevice extends CachedDevice<PowerReading> {
     readonly type: DeviceType = 'power';
 
     async getActualReading(): Promise<PowerReading> {
-        return fetch(`${process.env.PYTHON_DAEMON}/dts353f`)
-            .then(response => response.json() as Promise<DTS353FReading>)
-            .then(readings => this.toPowerReading(readings));
+        const reading = await fetch(`${process.env.PYTHON_DAEMON}/dts353f`)
+            .then(response => response.json() as Promise<DTS353FReading>);
+
+        const andledonReading = await DEVICE_REPOSITORY.findDevice<AndledonSmartMeterPowerDevice>('andledon-smart-meter', 'power')
+            .getReading()
+            .then(reading => reading.getReading())
+            .then(reading => reading.source as AndledonSmartMeterReading);
+
+        return this.toPowerReading(reading, andledonReading);
     }
 
     private toPowerReadingValues(reading: DTS353FReading, phase: 'l1' | 'l2' | 'l3'): PowerReadingValues {
@@ -21,10 +29,16 @@ export class RukbunkerSmartMeterPowerDevice extends CachedDevice<PowerReading> {
         return {power, voltage, amperage};
     }
 
-    private toPowerReading(reading: DTS353FReading): PowerReading {
+    private toPowerReading(reading: DTS353FReading, andledonReading: AndledonSmartMeterReading): PowerReading {
         const L1 = this.toPowerReadingValues(reading, 'l1');
         const L2 = this.toPowerReadingValues(reading, 'l2');
         const L3 = this.toPowerReadingValues(reading, 'l3');
+
+        if (andledonReading.current_readings.L3.power_redelivery > 0) {
+            // Rukbunker can redeliver on L3. So we are definitely redelivering.
+            L3.power *= -1;
+            L3.amperage *= -1;
+        }
 
         const total = {
             power: L1.power + L2.power + L3.power,
