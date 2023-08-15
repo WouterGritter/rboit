25ae748b04ccbebf3693094fba6a296532ac09f8
Wouter Gritter <woutergritter@gmail.com>
2023-08-15 16:57:09 +0200

    Make rboit-front a PWA

--- rboit-front/src/app/app.module.ts
+++ rboit-front/src/app/app.module.ts
@@ -44,6 +44,8 @@ import {
   NgxMatColorPickerModule
 } from "@angular-material-components/color-picker";
 import {MatInputModule} from "@angular/material/input";
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 const CanvasJSChart = CanvasJSAngularChart.CanvasJSChart;
 
@@ -89,7 +91,13 @@ const CanvasJSChart = CanvasJSAngularChart.CanvasJSChart;
     MatSliderModule,
     MatSelectModule,
     NgxMatColorPickerModule,
-    MatInputModule
+    MatInputModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
     providers: [
       { provide: MAT_COLOR_FORMATS, useValue: NGX_MAT_COLOR_FORMATS }
